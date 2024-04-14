ce0ecc1e81b1cd035a966c148bd78f1543c4b026
Danil <51288221+kyzinatra@users.noreply.github.com>
2024-04-14 16:29:26 +0300

    Правит стили. Уменьшает border-radius

--- src/service/url/qr.ts
+++ src/service/url/qr.ts
@@ -1,5 +1,8 @@
 import QRCode from "qrcode";
 
 export async function generateQR(link: string) {
-	return QRCode.toDataURL(link);
+	return QRCode.toDataURL(link, {
+		margin: 1,
+		scale: 9,
+	});
 }
